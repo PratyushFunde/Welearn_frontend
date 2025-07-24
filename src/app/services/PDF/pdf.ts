6f8d21fd001dbde78ea338c0ddb3e13a87e3e7b8
Pratyush Funde <pratyush@onelabventures.com>
2025-07-24 21:01:40 +0530

    Comments

---
+++ src/app/services/PDF/pdf.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+import * as pdfjsLib from "pdfjs-dist";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Pdf {
+  //Pdf extraction functions here
+  //Also add Model call in this service
+}
