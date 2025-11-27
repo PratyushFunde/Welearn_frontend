29d5ecfb2a145e3d10c3553790539a247dfd13c6
Pratyush Funde <pratyush@onelabventures.com>
2025-11-27 15:29:41 +0530

    Backend url change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true,
-  apiUrl: 'https://welearn-backend-1ln1.onrender.com/api',
+  apiUrl: '8pfzv2uti4.ap-south-1.awsapprunner.com/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8000/api', // Your Node backend
+  apiUrl: '8pfzv2uti4.ap-south-1.awsapprunner.com/api', // Your Node backend
 };  
